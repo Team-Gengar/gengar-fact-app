1fe334397a215ebda1daa7235f799a0b2ca29017
Chase Mann <chasemann@proton.me>
2023-02-14 18:19:35 -0500

    Updated homepage
    Co-authored-by: Horacio <hor.val@yahoo.com>
    Co-authored-by: Chase Mann <chasemann721@gmail.com>
    Co-authored-by: Johanna Merluza <johanna.merluza@gmail.com>
    Co-authored-by: Chris Oakes <ckoakes1@gmail.com>

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,45 @@
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import styles from '../../components/layout.module.css';
+
 export default function Home() {
   return (
     <>
-      <h1>Homepage</h1>
-      <h2>Most recent facts:</h2>
-      <h3>Fact 1:</h3>
-      <p>Cats have an extra organ that allows them to taste scents on the air, which is why your cat stares at you with her mouth open from time to time.</p>
-      <h3>Fact 2:</h3>
-      <p>Female cats have the ability to get pregnant when they are only 4 months old!</p>
-      <h3>Fact 3:</h3>
-      <p>Cats find it threatening when you make direct eye contact with them.</p>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Layout>
+        <div className={styles.feed}>
+          <h1>Homepage</h1>
+          <h2>Most recent facts:</h2>
+          <h3>Fact 1:</h3>
+          <p>Cats have an extra organ that allows them to taste scents on the air, which is why your cat stares at you with her mouth open from time to time.</p>
+          <h3>Fact 2:</h3>
+          <p>Female cats have the ability to get pregnant when they are only 4 months old!</p>
+          <h3>Fact 3:</h3>
+          <p>Cats find it threatening when you make direct eye contact with them.</p>
+          <h3>Fact 1:</h3>
+          <p>Cats have an extra organ that allows them to taste scents on the air, which is why your cat stares at you with her mouth open from time to time.</p>
+          <h3>Fact 2:</h3>
+          <p>Female cats have the ability to get pregnant when they are only 4 months old!</p>
+          <h3>Fact 3:</h3>
+          <p>Cats find it threatening when you make direct eye contact with them.</p>
+        </div>
+        <div className={styles.actionPanel}>
+          <div className={styles.navbar}>
+            <Image
+              src='/images/gear.png'
+              height={32}
+              width={32}
+              alt="Settings icon"
+            />
+          </div>
+          <div className={styles.generateFactPanel}>
+            Generate Fact Panel
+          </div>
+        </div>
+      </ Layout>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,6 +12,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ul>
+        <li>
+          <Link href='/home'>Homepage</Link>
+        </li>
+      </ul>
       {/* <main className={styles.main}> */}
       {/* <div className={styles.description}>
           <p>
