1f1e2b99dcc7668f20156aef41c9282fee337d85
Chase Mann <chasemann@proton.me>
2023-02-14 16:28:12 -0500

    Added placeholder home page
    Co-authored-by: Horacio <hor.val@yahoo.com>
    Co-authored-by: Chase Mann <chasemann721@gmail.com>
    Co-authored-by: Johanna Merluza <johanna.merluza@gmail.com>
    Co-authored-by: Chris Oakes <ckoakes1@gmail.com>

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Homepage</h1>
+      <h2>Most recent facts:</h2>
+      <h3>Fact 1:</h3>
+      <p>Cats have an extra organ that allows them to taste scents on the air, which is why your cat stares at you with her mouth open from time to time.</p>
+      <h3>Fact 2:</h3>
+      <p>Female cats have the ability to get pregnant when they are only 4 months old!</p>
+      <h3>Fact 3:</h3>
+      <p>Cats find it threatening when you make direct eye contact with them.</p>
+    </>
+  )
+}
